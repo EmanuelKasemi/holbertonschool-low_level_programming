fe565f2813f86d788bd74f37cf7af99e72468456
EmanuelKasemi <6247@holbertonstudents.com>
2023-02-27 04:05:22 -0800

    pointers, arrays and strings

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,26 @@
  */
 void rev_string(char *s)
 {
-	char tmp;
-	int i, length1, length2;
+	int i, c, k;
+	char *a, aux;
 
-	length1 = 0;
-	length2 = 0;
+	a = s;
 
-	while (s[length1] != '\0')
-		length1++;
-
-	length2 = length1 -1;
-	for (i = 0; i < length1; i++)
+	while (s[c] != '\0')
 	{
-		tmp = s[i];
-		s[i] = s[length2];
-		length2 -= 1;
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
 	}
 }

be1b39816146975e90947a2b4f33564c260010f4
EmanuelKasemi <6247@holbertonstudents.com>
2023-02-28 12:17:05 -0800

    pointers, arrays and strings

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,9 +11,11 @@ void rev_string(char *s)
 	int fcounter = 0;
 	int i;
 
-	while (s[fcounter] != '\0')
+	while
+	{
+	       	(s[fcounter] != '\0')
 		fcounter++;
-
+	}
 	for (i = 0; i < fcounter; i++)
 	{
 			fcounter--;
